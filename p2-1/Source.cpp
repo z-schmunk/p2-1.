54afa75ff80bef643659dc464868fa39f427c58a
z-schmunk <z-schmunk@onu.edu>
2024-09-05 09:15:33 -0400

    Add project files.

---
+++ p2-1/Source.cpp
@@ -0,0 +1,17 @@
+/*
+* FIle:		tempConverter.cpp
+* Author:   Zac Schmunk
+* Date:		9/5/2024
+*
+*Description: convert a Fahrenheit temperature to Celsius
+*/
+#include <iostream>
+#include<string>
+using namespace std;
+int main(void) {
+	string user_input;
+	cout << "hello my name is Hal" << endl;
+	cout << "what would you like me to do" << endl;
+	getline(cin, user_input);
+	cout << "I am sorry, I cannot do that." << endl;
+}
